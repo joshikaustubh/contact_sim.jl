a87932ec6dae57b7943e92fd3190f07518f66694
Kaustubh Joshi <55809570+joshikaustubh@users.noreply.github.com>
2024-05-10 12:08:48 -0400

    Update contacts.jl

--- src/contacts.jl
+++ src/contacts.jl
@@ -47,4 +47,35 @@ function get_block_contact_jacobian(q, body)
          [-rₓnₓ-rᵤnᵤ rₓnᵤ-rᵤnₓ]]
     
     return J, ϕ
+
+    
+end
+
+function get_rod_contact_jacobian(q, body)
+    xₜ = q[1]
+    yₜ = q[2]
+    θ  = q[3]
+    L = body.l
+
+    l = sqrt(2* (0.5*L)^2)
+    a = [xₜ+l*cos(θ), yₜ+l*sin(θ)]
+    b = [xₜ+l*cos(θ), yₜ+l*sin(θ)]
+    c = [xₜ-l*cos(θ), yₜ-l*sin(θ)]
+    d = [xₜ-l*cos(θ), yₜ-l*sin(θ)]
+
+    corners = [a, b, c, d]
+    min_index = argmin([a[2], b[2], c[2], d[2]])
+    xyc = corners[min_index]
+    ϕ = xyc[2]
+
+    n = [0, 1]
+    r = xyc - [xₜ, yₜ]
+    rₓnᵤ = r[1]*n[2]; rᵤnₓ = r[2]*n[1]
+    rₓnₓ = r[1]*n[1]; rᵤnᵤ = r[2]*n[2]
+
+    J = [[n[2] n[1]];
+         [-n[1] n[2]];
+         [-rₓnₓ-rᵤnᵤ rₓnᵤ-rᵤnₓ]]
+    
+    return J, ϕ
 end
